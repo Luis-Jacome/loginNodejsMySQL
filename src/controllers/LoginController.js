5809e6fd54f8a355b2ef51a2551bfb75de060bae
Luis-Jacome <161052974+Luis-Jacome@users.noreply.github.com>
2024-05-28 16:53:53 -0700

    Update LoginController.js

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,49 +1,84 @@
-function index(req, res) {
-  if (req.session.loggedin) {
-		// Output username
-    res.redirect('/');
-	
-  } else {
-    res.render('login/index');
+const bcrypt = require('bcrypt');
+const { redirect } = require('express/lib/response');
+
+function login(req, res) {
+    if (req.session.loggedin) {
+      res.redirect('/');
+    } else {
+      res.render('login/index');
+    }
   }
-}
+  
+  function register(req, res) {
+    if (req.session.loggedin) {
+      res.redirect('/');
+    } else {
+      res.render('login/register');
+    }
+  }
 
-function register(req, res) {
-  res.render('login/register');
-}
+  function storeUser(req, res){
+    const data = req.body;
+  
+    req.getConnection((err, conn) => {
+      conn.query('SELECT * FROM users WHERE email = ?', [data.email], (err, userdata) => {
+        if(userdata.length > 0) {
+          res.render('login/register', { error: 'Error: usuario existente'});
+        }else{
+          bcrypt.hash(data.password, 12).then(hash => {
+            data.password = hash;
 
-function auth(req, res) {
-	let email = req.body.email;
-	let password = req.body.password;
+            req.getConnection((err, conn) => {
+              conn.query('INSERT INTO users SET?', [data], (err,rows) =>{
+                req.session.loggedin = true;
+                req.session.name = data.name;
 
-  req.getConnection((err, conn) => {
-    conn.query('SELECT * FROM users WHERE email = ?', [email], (err, rows) => {
-      if(rows.length > 0) {
-        console.log(rows);
-      } else {
-        console.log('not');
-      }
-      /*
-      req.session.loggedin = true;
-	req.session.name = name;
-
-  res.redirect('/');*/
-      
+                res.redirect('/');
+              });
+            });
+          });
+        }
+      });
     });
-  });
-}
-
-function logout(req, res) {
-  if (req.session.loggedin) {
-    req.session.destroy();
   }
-  res.redirect('/');
-}
+  
+  function auth(req, res) {
+      const data = req.body;
 
+      req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM users WHERE email = ?', [data.email], (err, userdata) => {
 
-module.exports = {
-  index: index,
-  register: register,
-  auth: auth,
-  logout: logout,
-}
+          if(userdata.length > 0) {
+            userdata.forEach(element =>{
+              bcrypt.compare(data.password, element.password, (err, isMatch) =>{
+
+                if(!isMatch){
+                  res.render('login/index', { error: 'Error contraseña incorrecta'});
+                }else{
+                  req.session.loggedin = true;
+                  req.session.name = element.name;
+                  res.redirect('/');
+                }
+              });
+            });
+          }else{
+            res.render('login/index', { error: 'Error: usuario no existente'});
+          }
+        });
+      });
+  }
+  
+  function logout(req, res) {
+    if (req.session.loggedin) {
+      req.session.destroy();
+    }
+    res.redirect('/');
+  }
+  
+  module.exports = {
+    login,
+    register,
+    storeUser,
+    auth,
+    logout,
+  }
